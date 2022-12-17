f83f8498bd81b491a2c62bde8012314435cd28c9
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-12-17 18:54:46 -0300

    Radix added, context removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,6 @@
 import "../styles/globals.css";
 import type {AppProps} from "next/app";
 
-import {UIProvider} from "../context/uiContext";
-import {GiftsProvider} from "../context/giftsContext";
-
 export default function App({Component, pageProps}: AppProps) {
-  return (
-    <UIProvider>
-      <GiftsProvider>
-        <Component {...pageProps} />
-      </GiftsProvider>
-    </UIProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,60 @@
 import Image from "next/image";
-import {useContext} from "react";
-import {useForm, FormProvider, DefaultValues} from "react-hook-form";
+import {useEffect} from "react";
+import {useForm, DefaultValues, useFieldArray} from "react-hook-form";
 
-import {GiftAddModal} from "../components/GiftAddModal";
-import {GiftsContext} from "../context/giftsContext";
-import {UIContext} from "../context/uiContext";
-import {GiftsContextType, IGift} from "../types/gifts";
-import {UIContextType} from "../types/ui";
+import {AddGiftDialog} from "../components/AddGiftDialog";
+import {EditGiftDialog} from "../components/EditGiftDialog";
+import {IGift} from "../types/gifts";
 
-const formDefValues: DefaultValues<IGift> = {
-  name: "",
-  image: "",
-  receiver: "",
-  quantity: 0,
+const formDefValues: DefaultValues<{gifts: IGift[]}> = {
+  gifts: [],
 };
 
 export default function Home() {
-  const methods = useForm<IGift>({
+  const methods = useForm<{gifts: IGift[]}>({
     defaultValues: formDefValues,
   });
-  const {showModal, toggleModal} = useContext(UIContext) as UIContextType;
-  const {gifts, onDeleteGift, onDeleteAll, setCurrentGift} = useContext(
-    GiftsContext,
-  ) as GiftsContextType;
 
-  const {setValue} = methods;
+  const {append, update, remove, fields} = useFieldArray({
+    control: methods.control,
+    name: "gifts",
+  });
+
+  const handleDeleteGift = (index: number) => {
+    remove(index);
+  };
+
+  const handleDeleteAllGifts = () => {
+    methods.setValue("gifts", []);
+    window.localStorage.removeItem("gifts");
+  };
+
+  const handleAddGift = (values: IGift) => {
+    append({...values});
+    window.localStorage.setItem("gifts", JSON.stringify(methods.getValues().gifts));
+  };
+
+  const handleUpdateGift = (values: IGift, index: number) => {
+    update(index, {...values});
+    window.localStorage.setItem("gifts", JSON.stringify(methods.getValues().gifts));
+  };
+
+  useEffect(() => {
+    const localGifts = window.localStorage.getItem("gifts");
+
+    methods.setValue("gifts", localGifts !== null ? JSON.parse(localGifts) : []);
+  }, []);
 
   return (
-    <div className="min-h-screen bg-rose-400 -z-50">
-      <div className={`container flex items-center justify-center min-h-screen mx-auto`}>
-        <div
-          className={` ${
-            showModal ? "opacity-10" : ""
-          } flex flex-col gap-6 px-4 py-6 bg-white w-96 rounded-md border-2 border-rose-900`}
-        >
+    <div className="min-h-screen bg-red-900 -z-50">
+      <div className="container flex items-center justify-center min-h-screen mx-auto">
+        <div className="flex flex-col gap-6 px-4 py-6 bg-white border-2 rounded-md w-96 border-rose-900">
           <h1 className="text-4xl">Adviency Gifts</h1>
-          <button
-            className="px-2 py-1 text-white bg-red-900 border rounded-md boder-gray-900"
-            onClick={() => toggleModal(true)}
-          >
-            Agregar regalo
-          </button>
-          {gifts.length > 0 ? (
+          <AddGiftDialog handleAddGift={handleAddGift} />
+          {fields.length > 0 ? (
             <>
               <ul className="flex flex-col gap-2">
-                {gifts.map((gift, i) => (
+                {fields.map((gift, i) => (
                   <li key={i} className="flex items-center justify-between gap-2 overflow-hidden">
                     <Image
                       alt={gift.name}
@@ -57,22 +67,13 @@ export default function Home() {
                       <span className="flex-1 font-bold">{`${gift.name} x${gift.quantity}`}</span>
                       <span className="flex-1 text-xs text-gray-700">{gift.receiver}</span>
                     </div>
+                    <EditGiftDialog
+                      field={gift}
+                      handleUpdateGift={(values: IGift) => handleUpdateGift(values, i)}
+                    />
                     <button
                       className="px-2 py-1 text-white bg-red-900 border border-gray-900 rounded-md"
-                      onClick={() => {
-                        setValue("name", gift.name);
-                        setValue("image", gift.image);
-                        setValue("quantity", gift.quantity);
-                        setValue("receiver", gift.receiver);
-                        setCurrentGift(i);
-                        toggleModal(true);
-                      }}
-                    >
-                      E
-                    </button>
-                    <button
-                      className="px-2 py-1 text-white bg-red-900 border border-gray-900 rounded-md"
-                      onClick={() => onDeleteGift(i)}
+                      onClick={() => handleDeleteGift(i)}
                     >
                       X
                     </button>
@@ -81,7 +82,7 @@ export default function Home() {
               </ul>
               <button
                 className="py-1 text-white bg-red-900 border border-gray-900 rounded-md"
-                onClick={onDeleteAll}
+                onClick={handleDeleteAllGifts}
               >
                 Borrar todo
               </button>
@@ -90,11 +91,6 @@ export default function Home() {
             <span className="font-bold">No agregaste regalos</span>
           )}
         </div>
-        {showModal && (
-          <FormProvider {...methods}>
-            <GiftAddModal />
-          </FormProvider>
-        )}
       </div>
     </div>
   );
